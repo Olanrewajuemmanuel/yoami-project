df636b9d2af5b0c6d721823a5a3ab5e578c22d89
Olanrewajuemmanuel <63521462+Olanrewajuemmanuel@users.noreply.github.com>
2022-07-08 13:51:36 +0100

    - Filter option added

--- store_main/views.py
+++ store_main/views.py
@@ -1,24 +1,27 @@
 from django.utils import timezone
 from django.shortcuts import render
-from django.views import generic
 
 from .models import Item, Category
+from .globals import CATEGORIES
 
 # Create your views here.
+
 
 def index_view(request):
     template_name = 'store_main/index.html'
 
     context = {
         'items': Item.objects.filter(date_added__lte=timezone.now()).order_by("-date_added"),
+        'categories': CATEGORIES
     }
-    
+
     return render(request, template_name, context)
 
+
 def search_detail(request):
-    template_name = 'store_main/search.html'
+    template_name = 'store_main/search_details.html'
     context = {}
-    
+    context['categories'] = CATEGORIES
 
     try:
         item_name = request.GET['search']
@@ -26,9 +29,9 @@ def search_detail(request):
         min_price_filter = request.GET['min']
         max_price_filter = request.GET['max']
 
-        found_item = Item.objects.filter(title__icontains=item_name, category__category_name=item_category)
-        print(found_item)
-        context['found_item'] = found_item
+        found_items = Item.objects.filter(title__icontains=item_name, category__category_name=item_category, price__range=(
+            min_price_filter, max_price_filter)).order_by("-date_added")[:30]
+        context['found_items'] = found_items
     except (KeyError, Item.DoesNotExist):
         context['error_msg'] = "There was an error when searching the item, try again."
 
