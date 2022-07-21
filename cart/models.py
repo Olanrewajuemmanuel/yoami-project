38e2afd529096664fc21b3bfc04acac0be5bda80
Olanrewajuemmanuel <63521462+Olanrewajuemmanuel@users.noreply.github.com>
2022-07-21 18:40:23 +0100

    Cart BEGIN;

--- cart/models.py
+++ cart/models.py
@@ -7,8 +7,8 @@ from store_main.models import Item
 
 class CartItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    saved_item = models.ForeignKey(Item, on_delete=models.SET_NULL, null=True)
-    item_qty = models.CharField(default=0, max_length=100)
+    saved_item = models.OneToOneField(Item, on_delete=models.SET_NULL, null=True)
+    item_qty = models.IntegerField(default=0)
 
     def __str__(self):
         return self.saved_item.title
--- static/yoami/js/cart.js
+++ static/yoami/js/cart.js
@@ -1,15 +1,20 @@
-var user = JSON.stringify('{{ user }}');
-    console.log(user)
-    var cartBtns = document.querySelectorAll('#cart-btn');
-    cartBtns.forEach((btn) => {
-        btn.addEventListener("click", (e) => {
-            if (user === "AnonymousUser") {
-                e.preventDefault() // Do not go to any URL
-                return triggerModal()
-            }
-        })
-    })
-
-    function triggerModal() {
-        alert('You must be logged in to add to cart')
+var user_id = JSON.parse(document.getElementById("user_id").textContent);
+var cartBtns = document.querySelectorAll("#cart-btn");
+cartBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (!user_id) {
+      e.preventDefault(); // Do not go to any URL
+      return triggerModal();
+    } else {
+        updateCart(user_id)
     }
+  });
+});
+
+function triggerModal() {
+  alert("You must be logged in to add to cart");
+}
+
+function updateCart() {
+    
+}
