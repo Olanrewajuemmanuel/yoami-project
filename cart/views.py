df60b2d761084958ce9c3a9a897d63251846ddc8
Olanrewajuemmanuel <63521462+Olanrewajuemmanuel@users.noreply.github.com>
2022-07-26 17:34:17 +0100

    - working on bug

--- cart/views.py
+++ cart/views.py
@@ -29,17 +29,16 @@ def update_cart_view(request):
     item = Item.objects.get(pk=item_id)
     user_cart = Cart.objects.get(user=user)
     cart_item, created = CartItem.objects.get_or_create(cart=user_cart, saved_item=item)
-    
-    if cart_item.item_qty == 0:
-        cart_item.delete()
-
-
+  
     if action == 'add':
         cart_item.item_qty += 1
         cart_item.save()
     elif action == 'remove':
         cart_item.item_qty -= 1
         cart_item.save()
+    
+    if cart_item.item_qty == 0:
+        cart_item.delete()
     
     
 
